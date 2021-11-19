f76722b9aa2795bf0cb7f983f937aadf8c9ae7ea
William Reiske <wreiske@mieweb.com>
2021-11-19 14:10:21 -0700

    Fix tabs / Improve formatting

--- memleak-example.c
+++ memleak-example.c
@@ -3,30 +3,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-void badfun( int num, int leak )
+void badfun(int num, int leak) 
 {
-	char *c = malloc(100);
+  char * c = malloc(100);
 
-	sprintf(c,"Alloc: %c",num);
+  sprintf(c, "Alloc: %c", num);
 
-	if (leak) {
-      printf("Leaked!\n");
-		return;
-   }
-	free(c);
-	return;
+  if (leak) {
+    printf("Leaked!\n");
+    return;
+  }
+  free(c);
+  return;
 }
 
-int main( int argc, char* argv[] )
+int main(int argc, char * argv[]) 
 {
-    sleep( 5 ); 
-    int times= 0;
-    while( times < 10 ) {
-        badfun( times, times % 2); // every other one is a leak
-        times++;
-        sleep( 1 );
-    }
-    printf("Exiting with leaks\n");
+  sleep(5);
+  int times = 0;
+  while (times < 10) {
+    badfun(times, times % 2); // every other one is a leak
+    times++;
+    sleep(1);
+  }
+  printf("Exiting with leaks\n");
 
-    return 0;
+  return 0;
 }
